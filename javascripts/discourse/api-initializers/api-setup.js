0169f7556354563d54cec5acf6a59d106e91c298
denvergeeks <jen@denvergeeks.com>
2023-10-09 21:05:42 -0600

    Update api-setup.js

--- javascripts/discourse/api-initializers/api-setup.js
+++ javascripts/discourse/api-initializers/api-setup.js
@@ -2,9 +2,9 @@ import { apiInitializer } from "discourse/lib/api";
 import loadScript from "discourse/lib/load-script";
 import I18n from "I18n";
 
-async function applyInserted(element) {
-  const inserts = element.querySelectorAll("ins");
-  if (!inserted.length) {
+async function applyHighlighted(element) {
+  const highlighted = element.querySelectorAll("mark");
+  if (!highlighted.length) {
     return;
   }
 }
@@ -13,17 +13,17 @@ export default apiInitializer("0.11.1", (api) => {
   const { iconNode } = require("discourse-common/lib/icon-library");
   let icon = iconNode("highlighter");
   const currentLocale = I18n.currentLocale();
-  // I18n.translations[currentLocale].js.inserted_button_title = I18n.t(themePrefix("composer_inserted_button_title"));
-  // I18n.translations[currentLocale].js.composer.inserted_button_text = I18n.t(themePrefix("composer_inserted_button_text"));
-  I18n.translations[currentLocale].js.inserted_button_title = "Inserted Text";
+  // I18n.translations[currentLocale].js.highlighted_button_title = I18n.t(themePrefix("composer_highlighted_button_title"));
+  // I18n.translations[currentLocale].js.composer.highlighted_button_text = I18n.t(themePrefix("composer_highlighted_button_text"));
+  I18n.translations[currentLocale].js.highlighted_button_title = "Highlighted Text";
   I18n.translations[currentLocale].js.composer.this = "this";
-  // I18n.translations[currentLocale].js.composer.insert_button_text = "Inserted Text";
+  // I18n.translations[currentLocale].js.composer.highlighted_button_text = "Highlighted Text";
 
   api.modifyClass("controller:composer", {
-    pluginId: "inserted",
+    pluginId: "highlighted",
     actions: {
-      insertedButton() {
-        this.get("toolbarEvent").applySurround("\n" + `<ins>` + "\n</ins>\n");
+      highlightedButton() {
+        this.get("toolbarEvent").applySurround("\n" + `<mark>` + "\n</mark>\n");
       },
     },
   });
@@ -31,23 +31,23 @@ export default apiInitializer("0.11.1", (api) => {
   // add button to the toolbar
   api.onToolbarCreate((toolbar) => {
     toolbar.addButton({
-      id: "composer_inserted_button",
+      id: "composer_highlighted_button",
       group: "extras",
       icon: "highlighter",
       shortcut: "N",
       preventFocus: true,
       trimLeading: true,
-      title: "inserted_button_title",
-      // perform: e => e.applySurround('<span>[wrap=insert]', '[/wrap]</span>', 'this')
-      perform: (e) => e.applySurround("<ins>", "</ins>", "this"),
+      title: "highlighted_button_title",
+      // perform: e => e.applySurround('<span>[wrap=highlighted]', '[/wrap]</span>', 'this')
+      perform: (e) => e.applySurround("<mark>", "</mark>", "this"),
     });
   });
 
   api.decorateCookedElement(
     async (elem, helper) => {
       const id = helper ? `post_${helper.getModel().id}` : "composer";
-      applyInserted(elem, id);
+      applyHighlighted(elem, id);
     },
-    { id: "wrap-insert" }
+    { id: "wrap-highlighted" }
   );
 });
